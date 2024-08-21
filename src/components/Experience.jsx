e121285df107027d00da127f0f018876551c947c
kforsl <forslund95@live.com>
2024-08-21 10:46:40 +0200

    remove cv button

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,11 +2,6 @@ import ExperianceCard from "./ExperianceCard";
 import { useEffect, useState } from "react";
 import Title from "./Title";
 import { experience } from "../data/data";
-import { IoMdDownload } from "react-icons/io";
-
-const openCVinNewTab = () => {
-  window.open("../assets/CV.pdf", "_blank");
-};
 
 function Experience() {
   const [experiences, setExperiences] = useState([]);
@@ -18,15 +13,6 @@ function Experience() {
   return (
     <section className="relative">
       <Title title={"Erfarenheter"} />
-      <button
-        className="group absolute left-auto right-6 top-0 bg-grayish px-2 text-white"
-        onClick={openCVinNewTab}
-      >
-        <p className="absolute left-1/2 top-full -translate-x-1/2 text-nowrap opacity-0 transition duration-300 group-hover:opacity-100">
-          Öppna CV
-        </p>
-        <IoMdDownload size={"32px"} className="group-hover:animate-pulse" />
-      </button>
       <section
         className={`mx-auto grid text-white md:grid-cols-2 md:grid-rows-${experiences.length + 1} md:gap-y-4`}
       >
